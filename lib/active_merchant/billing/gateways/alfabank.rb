106318456182cc0893b92c2da8d0829f49aa7bca
Alabama Air <alabama.air@gmail.com>
2017-12-18 18:21:57 +0800

    Add custom urls for Alfabank gateway

--- lib/active_merchant/billing/gateways/alfabank.rb
+++ lib/active_merchant/billing/gateways/alfabank.rb
@@ -32,6 +32,8 @@ module ActiveMerchant #:nodoc:
       # * <tt>:secret</tt> -- The Alfabank API secret (REQUIRED)
       def initialize(options = {})
         requires!(options, :account, :secret)
+        @test_url = options[:test_url] if options[:test_url]
+        @live_url = options[:live_url] if options[:live_url]
         super
       end
 
